632d2bedf720565b51b690033a3116d50188aa2a
Jyothi Sumer Goud Maduru <jmaduru@vclvm178-161.vcl.ncsu.edu>
2023-04-01 11:59:03 -0400

    Added Velvet

--- kublet/main.go
+++ kublet/main.go
@@ -1,47 +1,16 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os/exec"
 	"strings"
+	"net/http"
 	"time"
-
 	// "strconv"
 
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
-	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
-
-var URL = "http://152.7.179.7:8086"
-var TOKEN = "TOKEN"
-var ORG = "NCSU"
-var BUCKET = "ADS"
-
-func AddMetricEntry(cpu int, memory int) string{
-    // Create a new client using an InfluxDB server base URL and an authentication token
-	fmt.Println("Creating URL")
-    client := influxdb2.NewClient(URL, TOKEN)
-	fmt.Println("Creating WriteAPI")
-    // Use blocking write client for writes to desired bucket
-    writeAPI := client.WriteAPIBlocking(ORG, BUCKET)
-    // Create point using full params constructor
-    p := influxdb2.NewPoint("metric",
-	map[string]string{"type": "stats"},
-	map[string]interface{}{"cpu": cpu, "mem": memory},
-	time.Now())
-    // write point immediately
-	fmt.Println("Writing started")
-    writeAPI.WritePoint(context.Background(), p)
-	fmt.Println("Writing completed")
-
-    // Ensures background processes finishes
-    client.Close()
-	return "Added Entry in DB"
-}
-
 
 func createContainer(port string, containerName string) string {
 	portBind := port + ":8000"
@@ -118,9 +87,7 @@ func helloWorldRoute(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World")
 }
 
-func addMetricEntryRoute(c echo.Context) error {
-	return c.String(http.StatusOK, AddMetricEntry(50, 100))
-}
+
 
 func metricsRoute(c echo.Context) error {
 	return c.String(http.StatusOK, getCPUMetric())
@@ -143,7 +110,6 @@ func main() {
 	e.GET("/delete/container/:id", deleteContainerRoute)
 	e.GET("/count/containers", runningContainersCountRoute)
 	e.GET("/metrics", metricsRoute)
-	e.GET("/addMetric", addMetricEntryRoute)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
---
+++ velvet/go.mod
@@ -0,0 +1,23 @@
+module github.com/J-Sumer/AutoScaler/velvet
+
+go 1.18
+
+require (
+	github.com/influxdata/influxdb-client-go/v2 v2.12.3
+	github.com/labstack/echo/v4 v4.10.2
+)
+
+require (
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
